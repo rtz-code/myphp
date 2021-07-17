0b19bebf730e63657e0d0823f279d91d2d31df51
Ricardo Tortoza <rtz.code@gmail.com>
2021-07-17 01:57:17 -0400

    practica

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My practice in php</title>
+</head>
+<body>
+
+<?php
+
+    $nombre_de_producto_por_defecto = "Producto";
+    $nombre_producto = "{$nombre_de_producto_por_defecto}s en oferta";
+    echo $nombre_producto; // imprime: Productos en oferta
+
+?>
+
+</body>
+</html>
